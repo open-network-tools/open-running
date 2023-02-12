1a227da58ca7edc498d9f6baa8d166ee41cefd53
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2023-02-12 15:58:35 +0100

    Update OpenRunning #patch

--- src/OpenRunning.php
+++ src/OpenRunning.php
@@ -2,12 +2,18 @@
     namespace OpenNetworkTools;
 
     use OpenNetworkTools\Interfaces\OpenRunningInterface;
+    use OpenNetworkTools\OpenRunning\Interfaces;
 
     class OpenRunning implements OpenRunningInterface {
 
         private $interfaces;
 
         public function __construct() {
+            $this->interfaces = new Interfaces();
+        }
+
+        public function getInterfaces(): Interfaces {
+            return $this->interfaces;
         }
 
     }
