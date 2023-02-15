2ee30252bd6212623991a2b3dbaff746e0af1a78
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2023-02-15 20:36:59 +0100

    Update OpenReport #patch

--- src/OpenRunning/Interfaces.php
+++ src/OpenRunning/Interfaces.php
@@ -27,6 +27,30 @@
             return $this->ethernet[$fpc][$pic][$port];
         }
 
+        public function countEthernet($mode = null, $fpc = null, $pic = null){
+            $count = 0;
+
+            foreach ($this->ethernet as $kfpc => $vfpc){
+                if(is_null($fpc) || $kfpc == $fpc){
+                    foreach ($vfpc as $kpic => $vpic){
+                        if(is_null($pic) || $kpic == $pic){
+                            foreach ($vpic as $vport){
+                                if(is_null($mode)) $count++;
+                                elseif($mode == "adminUp") $vport->getAdminStatus() ? $count++ : null;
+                                elseif($mode == "adminDown") !$vport->getAdminStatus() ? $count++ : null;
+                                elseif($mode == "operUp") $vport->getOperStatus() ? $count++ : null;
+                                elseif($mode == "operDown") !$vport->getOperStatus() ? $count++ : null;
+                                else null;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return $count;
+        }
+
+        /**
         public function countEthernet($mode = null){
             if($mode == "adminUp"){
                 $count = 0;
@@ -122,14 +146,15 @@
                 return $count;
             }
         }
+         * */
 
         public function getEthernet($fpc = null, $pic = null, $port = null) {
             if(is_numeric($fpc) && is_numeric($pic) && is_numeric($port)){
-                if(!array_key_exists($fpc, $this->ethernet)) $this->addEthernet($fpc, $pic, $port);
-                if(!array_key_exists($pic, $this->ethernet[$fpc])) $this->addEthernet($fpc, $pic, $port);
-                if(!array_key_exists($port, $this->ethernet[$fpc][$pic])) $this->addEthernet($fpc, $pic, $port);
+                if(array_key_exists($fpc, $this->ethernet)) return $this->ethernet[$fpc];
+                if(array_key_exists($pic, $this->ethernet[$fpc])) return $this->ethernet[$fpc][$pic];
+                if(array_key_exists($port, $this->ethernet[$fpc][$pic])) return $this->ethernet[$fpc][$pic][$port];
 
-                return $this->ethernet[$fpc][$pic][$port];
+                return $this->ethernet;
             } else return $this->ethernet;
         }
 
